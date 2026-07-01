20dbd07b50855b73ce80151ab0b0d54d9c9a6fe3
qingshanls <y3205731642@outlook.com>
2026-07-01 17:16:36 +0800

    update theme

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,3 +8,19 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # uv
 fish_add_path "/home/qsls/.local/bin"
+
+# 开启代理
+function pon
+    set -gx http_proxy http://127.0.0.1:7890
+    set -gx https_proxy http://127.0.0.1:7890
+    set -gx all_proxy socks5://127.0.0.1:7890
+    echo "HTTP/HTTPS Proxy on"
+end
+
+# 关闭代理
+function pof
+    set -e http_proxy
+    set -e https_proxy
+    set -e all_proxy
+    echo "HTTP/HTTPS Proxy off"
+end
