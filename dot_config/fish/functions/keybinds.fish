05716a7415ed4e987fa89ce5ab833e4a9a822b96
qingshanls <y3205731642@outlook.com>
2026-07-20 14:23:24 +0800

    dotfiles

--- dot_config/fish/functions/keybinds.fish
+++ dot_config/fish/functions/keybinds.fish
@@ -4,7 +4,23 @@ function y
 	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
 		builtin cd -- "$cwd"
 	end
-	rm -f -- "$tmp"
+	command rm -f -- "$tmp"
 end
 
-abbr -a ff fastfetch
+# 开启代理
+function pon
+    set -gx http_proxy http://127.0.0.1:7890
+    set -gx https_proxy http://127.0.0.1:7890
+    set -gx all_proxy socks5://127.0.0.1:7890
+    echo "HTTP/HTTPS Proxy on"
+end
+
+# 关闭代理
+function pof
+    set -e http_proxy
+    set -e https_proxy
+    set -e all_proxy
+    echo "HTTP/HTTPS Proxy off"
+end
+
+alias ff='fastfetch'
